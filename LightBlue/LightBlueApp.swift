511059b91847e33ce39a9f9f85f2adb5cffb256d
mamong <chyy.meng@gmail.com>
2026-04-16 22:55:06 +0800

    Initial Commit

---
+++ LightBlue/LightBlueApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightBlueApp.swift
+//  LightBlue
+//
+//  Created by Marco on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct LightBlueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
